db92120c001af19b58804e6cf12020a8cfe50f07
Heesom <heesom.hs@gmail.com>
2023-01-17 13:33:55 +0900

    solve: 1288 in d2

--- D2/1288_countSheep.py
+++ D2/1288_countSheep.py
@@ -1,24 +1,44 @@
+# Count the amount that is a multiple of N
+# At least how many times i have to count to see all the digits from 0 to 9 in each digit of the previously counted numbers?
 T = int(input())
 
 for test_case in range(1, T+1):
     num = int(input())
-
-    isStillRunning = True
-    zeroToNine = [num]
-    count = 0
+    origin_num = num
+    zeroToNine = []
+    is_not_enough = True
+    count = 1
+    multiple = 2
     #check if zeroToNine has all number in range 0~9
-    while True:
-        zeroToNine.append(num)
-        zeroToNine = set(zeroToNine)
-        zeroToNine = sorted(zeroToNine)
-        zeroToNine = str(zeroToNine)
+    while is_not_enough:
         
-        num *= 2
-        count += 1
+        
+        # Divides the number in units(1's digit)
+        is_more_then_onedigit = True
+        tmp_num = num
+        
+        while is_more_then_onedigit:
+            
+            remainder = tmp_num % 10
+            zeroToNine.append(remainder)
+            tmp_num = tmp_num // 10
 
-        if zeroToNine == '0123456789':
+            if tmp_num == 0:
+                is_more_then_onedigit = False
+
+
+        zeroToNine = set(zeroToNine)
+        zeroToNine = sorted(list(zeroToNine))
+        tmp = ''.join(str(i) for i in zeroToNine)
+        print(count, num)
+       
+        if tmp == '0123456789':
             break
         
-    print(f'#{test_case} {count}')
+        count += 1
+        num = origin_num * multiple
+        multiple += 1
+        
+    print(f'#{test_case} {num}')
     
 
