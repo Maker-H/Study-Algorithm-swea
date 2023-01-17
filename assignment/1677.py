50a901e35281e93823d43f841da92020aa75839f
Heesom <heesom.hs@gmail.com>
2023-01-17 17:47:34 +0900

    TODO: 1928 d2 - have to review

---
+++ assignment/1677.py
@@ -0,0 +1,11 @@
+todos = [("Python Homework", 3), ("Assay", 4), ("Vacation", 100)]
+
+todo_work = input()
+remain_date = int(input())
+
+todo = (todo_work, remain_date)
+
+todos.append(todo)
+
+
+print(todos)
