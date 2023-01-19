a455661e03765f4a7b92153b7d3f4cbaed11cb45
Heesom <heesom.hs@gmail.com>
2023-01-19 10:04:10 +0900

    solve: 1954 (aka snail) in d2!!!!!!

--- D2/1954_printSnail.py
+++ D2/1954_printSnail.py
@@ -1,9 +1,13 @@
-# Get current location by tuple and set goal by R
-def goRight(snail, current, R):
+# The numbers from 1 to N*N are arranged clockwise.
+# Take an integer N as input and print a snail of size N.
+
+
+# Get current location by tuple and set goal by G
+def goRight(snail, current, G):
     l, r = current
 
     count = snail[l][r]
-    for move in range(R):
+    for move in range(G):
         r += 1
         count += 1
         snail[l][r] = count
@@ -11,73 +15,75 @@ def goRight(snail, current, R):
     return (l, r)
         
 
-def goLeft(snail, current, R):
+def goLeft(snail, current, G):
     l, r = current
 
     count = snail[l][r]
-    for move in range(R):
+    for move in range(G):
         r -= 1
         count += 1
         snail[l][r] = count
 
     return (l, r)
     
-def goUp(snail, current, R):
+def goUp(snail, current, G):
     l, r = current
 
     count = snail[l][r]
-    for move in range(R):
+    for move in range(G):
         l -= 1
         count += 1
         snail[l][r] = count
 
     return (l, r) 
 
-def goDown(snail, current, R):
+def goDown(snail, current, G):
     l, r = current
 
     count = snail[l][r]
-    for move in range(R):
+    for move in range(G):
         l += 1
         count += 1
         snail[l][r] = count
 
     return (l, r)
 
+def my_print(test_case, snail):
+    print(f'#{test_case}')
+    for oneline in snail:
+        tmp = " ".join(list(map(str, oneline)))
+        print(tmp)
+            
+# -------------------------------------------------------------
+
 T = int(input())
 
 for test_case in range(1, T+1):
     N = int(input())
     snail_N = N
-    snail = [[0 for j in range(N)] for i in range(N)]
 
+    # Initialize snail list
+    snail = [[0 for j in range(N)] for i in range(N)]
+    snail[0][0] = 1
     first_location = (0, 0)
 
+    # Have to move Right by 'arraySize -1'
     snail_N -= 1
+
+    # Have to move R first and loop by DLUR 
     location = goRight(snail, first_location, snail_N)
-    for i in range(snail_N // 2):
+    while True:
         if snail_N == 0 :
             break
-        locaton = goDown(snail, location, 2)
-        locaion = goLeft(snail, location, 2)
+        location = goDown(snail, location, snail_N)
+        location = goLeft(snail, location, snail_N)
         snail_N -= 1
         
         if snail_N == 0 :
             break
-        locaton = goUp(snail, location, 1)
-        locaion = goRight(snail, location, 1)
+        location = goUp(snail, location, snail_N)
+        location = goRight(snail, location, snail_N)
         snail_N -= 1
 
-    print(snail)
-    # # N = 4일때
-    # location = goRight(snail, first_location, 3)
-    # locaiton = goDown(location, 3)
-    # location = goLeft(location, 3)
-    # locaiton = goUp(location, 2)
-    # location = goRight(location,2)
-    # locaiton = goDown(location, 1)
-    # location = goLeft(location, 1)
+    my_print(test_case, snail)
     
-    
-    #howEnd??
-
