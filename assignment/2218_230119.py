634df45df791cfd19f2066c9480343333e2723ba
Heesom <heesom.hs@gmail.com>
2023-01-20 09:11:20 +0900

    solve: 2218 in assignment by myself ^^

--- assignment/2218_230119.py
+++ assignment/2218_230119.py
@@ -1,29 +1,12 @@
-words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+words = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
 
+answer = {}
 
+for word in words:
+    sorted_word = "".join(sorted(list(word)))
+    if sorted_word not in answer:
+        answer[sorted_word] = [word]
+    elif sorted_word in answer:
+        answer[sorted_word].append(word)
 
-idx = 0
-sorted_words = []
-
-while idx != len(words):
-    sorted_words.append(sorted(list(words[idx])))
-    idx += 1
-
-anagram = {}
-count = 0
-
-for word in sorted_words:
-    if word not in sorted_words:
-        anagram[word] = (count)
-    elif word in sorted_words:
-        anagram[word].update(count)
-    count += 1
-    
-
-
-print(type(anagram))
-
-# for key in anagram.keys():
-#     anagram.get(key)
-    
-
+print(answer)
