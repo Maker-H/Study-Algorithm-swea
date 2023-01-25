312fc23df96219e2d102ef05905e74a5ee152b1d
Heesom <heesom.hs@gmail.com>
2023-01-25 13:28:18 +0900

    prof: 2217
    #1

--- assignment/2217_230119.py
+++ assignment/2217_230119.py
@@ -7,5 +7,19 @@ while count != (len(words)-1):
     else:
         print(count)
         break
-    
-    
+
+# Professor
+for idx in range(len(words)-1):
+	if words[idx][-1] != words[idx+1][0] or words[i] in words[:idx]:
+		print(f'{i+1}번째 탈락자가 탈락하였습니다')
+		break
+else:
+	print('축하합니다. 아무도 탈락하지 않으셨습니다.')
+
+for idx in range(len(words)-1):
+	if words[idx][-1] != words[idx+1][0] or words[i] in words[:idx]:
+		flag = False
+		print(f'{i+1}번째 탈락자가 탈락하였습니다')
+		break
+if flag:
+	print('축하합니다. 아무도 탈락하지 않으셨습니다.')
