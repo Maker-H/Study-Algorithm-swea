072dea7e12128851bda13c6841dad4a823010616
Heesom <heesom.hs@gmail.com>
2023-02-02 09:01:39 +0900

    refac: edit .txt files name

--- assignment/230201/C1_View.py
+++ assignment/230201/C1_View.py
@@ -1,19 +1,38 @@
 import sys
 
+def get_max_building_house(current_idx, tower_map):
+
+    four_tower = [0 for i in range(4)]
+
+    four_tower[0] = tower_map[current_idx - 1]
+    four_tower[1] = tower_map[current_idx - 2]
+    four_tower[2] = tower_map[current_idx + 1]
+    four_tower[3] = tower_map[current_idx + 2]
+
+    max = 0
+
+    for one in four_tower:
+        if max < one:
+            max = one
+
+    house = tower_map[current_idx] - max
+
+    if house < 0:
+        return 0
+    else:
+        return house
+
 
 sys.stdin = open("C1_input.txt", "r")
 
-T = int(input())
+T = 10
 
 for test_case in range(1, T + 1):
     N = int(input())
     tower_map = list(map(int, input().split()))
 
-    for idx in range(len(tower_map)+1):
-        if 1 <= tower_map[idx]:
-            hight = idx - (1의 idx) -1
+    houses = 0
+    for idx in range(2, len(tower_map) - 2):
+        houses += get_max_building_house(idx, tower_map)
 
-
-
-
-
+    print(f'#{test_case} {houses}')
